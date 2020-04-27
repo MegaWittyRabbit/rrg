7544ac1bf83a0a98700fd6937b002b9f8aa3e6c3
Dashbold <bda227@uky.edu>
2020-04-27 01:08:12 -0400

    lab7 updated
    elevation folder is added to the repository

---
+++ elevation/potree/pointclouds/index/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 1031182,
+    "boundingBox": {
+        "lx": 5516679.75,
+        "ly": 3833918.16,
+        "lz": 738.48,
+        "ux": 5518679.67,
+        "uy": 3835918.08,
+        "uz": 2738.3999999999255
+    },
+    "tightBoundingBox": {
+        "lx": 5516679.75,
+        "ly": 3833918.16,
+        "lz": 738.48,
+        "ux": 5518679.65,
+        "uy": 3835918.08,
+        "uz": 1930.17
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 17.319814682006837,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
